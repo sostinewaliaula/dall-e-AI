42611d20d4c6106c2868507e3ef7007352b57a9d
sostinewaliaua <sostinewaliaula@gmail.com>
2023-01-28 14:29:13 +0000

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,13 @@
+// import download from './download.png';
+// import logo from './logo.svg';
+// import preview from './preview.png';
+
+// export {
+//   download,
+//   logo,
+//   preview,
+// };
+
+export { default as Logo } from "./logo.svg";
+export { default as download } from "./download.png";
+export { default as preview } from "./preview.png";
---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Card } from "./Card";
+export { default as FormField } from "./FormField";
+export { default as Loader } from "./Loader";
