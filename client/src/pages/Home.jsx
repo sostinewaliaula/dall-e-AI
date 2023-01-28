2b283ac096788daf2756aeaa81c360d87f1bbdf3
sostinewaliaula <83455882+sostinewaliaula@users.noreply.github.com>
2023-01-28 15:18:26 +0000

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
     try {
       const response = await fetch(
-        "https://dalle-arbb.onrender.com/api/v1/post",
+        "https://dall-e-ai-b3jg.onrender.com/api/v1/post",
         {
           method: "GET",
           headers: {
